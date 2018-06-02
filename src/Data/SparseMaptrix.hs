078ec2768e2d2c3e4653a93b59876ba9ba56647a
k32 <skmshj@gmail.com>
2018-06-02 21:31:36 +0200

    Some random ramblings

--- src/Data/SparseMaptrix.hs
+++ src/Data/SparseMaptrix.hs
@@ -1,53 +1,72 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeFamilies, ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeFamilies #-}
 {- Extremely stupid and inefficient implementation of
    row-major sparse matrix indexed by arbitrary 
    instances of Ord class instead of uints
 -}
 module Data.SparseMaptrix (
                             Maptrix
-                          -- , insert
-                          -- , updateRow
-                          -- , (!)
-                          , SlicePlaceholder(..)
-                          -- , _
+                          , Slice(..)
+                          , update
+                          , H(..)
+                          , S(..)
                           , test
                           ) where
 
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import Data.Void
 import Data.Traversable
 
-data SlicePlaceholder = SlicePlaceholder
+data H = H
 
-_m = SlicePlaceholder
+newtype S a = S a
 
 newtype Maptrix k1 k2 v = Maptrix (M.Map k1 (M.Map k2 v))
 
 type family Slice k1 k2 v i :: * where
-  Slice k1 k2 v (k1, SlicePlaceholder) = M.Map k2 v
-  Slice k1 k2 v (SlicePlaceholder, k2) = M.Map k1 v
-  Slice k1 k2 v (k1, k2) = v
+  Slice k1 k2 v (S k1, H) = M.Map k2 v
+  Slice k1 k2 v (H, S k2) = M.Map k1 v
+  Slice k1 k2 v (S k1, S k2) = v
 
 class (Ord k1, Ord k2, Num v) => MSlice k1 k2 v s where
-  (!) :: Maptrix k1 k2 v -> s -> Slice k1 k2 v s
+  (!) :: Maptrix k1 k2 v    -- ^ Map
+      -> s                  -- ^ Index 
+      -> Slice k1 k2 v s
 
+  insert :: s               -- ^ Index
+         -> Slice k1 k2 v s -- ^ Value to insert
+         -> Maptrix k1 k2 v -- ^ Initial map
+         -> Maptrix k1 k2 v
 
-instance (Ord k1, Ord k2, Num v) => MSlice k1 k2 v (k1, k2) where
+instance (Ord k1, Ord k2, Num v) => MSlice k1 k2 v (S k1, S k2) where
 -- instance (Num v) => MSlice Int Int v (Int, Int) where
-  (Maptrix m) ! (k1, k2) = case k1 `M.lookup` m of
-                             Just x -> case k2 `M.lookup` x of
-                                         Just v -> v
-                                         Nothing -> 0
-                             Nothing -> 0
+  (Maptrix m) ! (S k1, S k2) = case k1 `M.lookup` m of
+                                 Just x -> case k2 `M.lookup` x of
+                                             Just v -> v
+                                             Nothing -> 0
+                                 Nothing -> 0
 
-instance (Ord k1, Ord k2, Num v) => MSlice k1 k2 v (k1, SlicePlaceholder) where
-  (Maptrix m) ! (k1, _) = case k1 `M.lookup` m of
+  insert (S k1, S k2) elem (Maptrix m) =
+      Maptrix $ M.insertWith (const $ M.insert k2 elem)
+                             k1
+                             (M.singleton k2 elem)
+                             m
+
+instance (Ord k1, Ord k2, Num v) => MSlice k1 k2 v (S k1, H) where
+  (Maptrix m) ! (S k1, _) = case k1 `M.lookup` m of
                             Just a -> a
                             Nothing -> M.empty
+                                       
+  insert (S k1, H) row (Maptrix m) = Maptrix $ M.insert k1 row m
+
+update :: (Ord k1, Ord k2, Num v)
+       => Maptrix k1 k2 v
+       -> [(k1, [(k2, v)])]
+       -> Maptrix k1 k2 v
+update = undefined
 
 test =
-  let m = (Maptrix M.empty) :: Maptrix Int Int Int
-      s1 = (m ! (1 :: Int, SlicePlaceholder)) :: M.Map Int Int
-      s2 = (m ! (1 :: Int, 2 :: Int)) :: Int
+  let m = (Maptrix M.empty) :: Maptrix String String Int
+      s1 = (m ! (S "", S "1")) :: Int
+      s2 = (m ! (S "", H)) :: M.Map String Int
   in
     True
