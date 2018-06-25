b20a7a1ae2184b712d95b3655f75169378735cdd
k32 <10274441+k32@users.noreply.github.com>
2018-06-26 00:09:31 +0200

    It's usable, but verification is still broken

--- src/Data/SparseMaptrix.hs
+++ src/Data/SparseMaptrix.hs
@@ -1,20 +1,28 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances,
+             FlexibleContexts, TypeFamilies #-}
 {- Extremely stupid and inefficient implementation of
-   row-major sparse matrix indexed by arbitrary 
+   row-major sparse matrix indexed by arbitrary
    instances of Ord class instead of uints
 -}
 module Data.SparseMaptrix (
                             Maptrix
                           , Slice(..)
+                          , MSlice(..)
                           , update
+                          , updateWith
                           , H(..)
                           , S(..)
-                          , test
+                          , new
+                          , toList
+                          , fromList
+                          , rows
                           ) where
 
 import qualified Data.Map.Strict as M
 import Data.Void
 import Data.Traversable
+import Data.List (foldl', groupBy, sortBy)
+import Data.Function (on)
 
 data H = H
 
@@ -29,7 +37,7 @@ type family Slice k1 k2 v i :: * where
 
 class (Ord k1, Ord k2, Num v) => MSlice k1 k2 v s where
   (!) :: Maptrix k1 k2 v    -- ^ Map
-      -> s                  -- ^ Index 
+      -> s                  -- ^ Index
       -> Slice k1 k2 v s
 
   insert :: s               -- ^ Index
@@ -37,7 +45,7 @@ class (Ord k1, Ord k2, Num v) => MSlice k1 k2 v s where
          -> Maptrix k1 k2 v -- ^ Initial map
          -> Maptrix k1 k2 v
 
-instance (Ord k1, Ord k2, Num v) => MSlice k1 k2 v (S k1, S k2) where
+instance (Ord k1, Ord k2, Eq v, Num v) => MSlice k1 k2 v (S k1, S k2) where
 -- instance (Num v) => MSlice Int Int v (Int, Int) where
   (Maptrix m) ! (S k1, S k2) = case k1 `M.lookup` m of
                                  Just x -> case k2 `M.lookup` x of
@@ -45,24 +53,76 @@ instance (Ord k1, Ord k2, Num v) => MSlice k1 k2 v (S k1, S k2) where
                                              Nothing -> 0
                                  Nothing -> 0
 
+  insert (S k1, S k2) 0 (Maptrix m) = Maptrix $ M.update f k1 m
+    where f m = case M.delete k2 m of
+                  m' | M.null m' -> Nothing
+                     | otherwise -> Just m'
   insert (S k1, S k2) elem (Maptrix m) =
-      Maptrix $ M.insertWith (const $ M.insert k2 elem)
-                             k1
-                             (M.singleton k2 elem)
-                             m
+    Maptrix $ M.insertWith (const $ M.insert k2 elem)
+                           k1
+                           (M.singleton k2 elem)
+                           m
+
+new :: Maptrix k1 k2 v
+new = Maptrix $ M.empty
 
 instance (Ord k1, Ord k2, Num v) => MSlice k1 k2 v (S k1, H) where
   (Maptrix m) ! (S k1, _) = case k1 `M.lookup` m of
                             Just a -> a
                             Nothing -> M.empty
-                                       
+
   insert (S k1, H) row (Maptrix m) = Maptrix $ M.insert k1 row m
 
-update :: (Ord k1, Ord k2, Num v)
+
+updateWith :: (Ord k1, Ord k2, Eq v, Num v)
+           => (v -> v -> v)
+           -> Maptrix k1 k2 v
+           -> [((k1, k2), v)]
+           -> Maptrix k1 k2 v
+updateWith f (Maptrix m0) l =
+  let
+    nullToNothing 0 = Nothing
+    nullToNothing n = Just n
+
+    groups = groupBy ((==) `on` (fst . fst)) $ sortBy (compare `on` fst) l
+    groups' = [(k1, l') | g <- groups
+                        , let k1 = fst $ fst $ head g
+                              l' = [(k2, v) | ((_, k2), v) <- g]
+                        ]
+
+    doRow l row = if M.null row'
+                  then Nothing
+                  else Just row'
+      where row' = foldr doElem row l -- ineffective
+
+    doElem (k2, v) = M.update (\v0 -> nullToNothing $ f v0 v) k2
+  in
+    Maptrix $ foldr (\(k1, l) -> M.update (doRow l) k1) m0 groups'
+
+update :: (Ord k1, Ord k2, Eq v, Num v)
        => Maptrix k1 k2 v
-       -> [(k1, [(k2, v)])]
+       -> [((k1, k2), v)]
        -> Maptrix k1 k2 v
-update = undefined
+update = updateWith (\_ a -> a)
+
+toList :: (Ord k1, Ord k2)
+       => Maptrix k1 k2 v
+       -> [((k1, k2), v)]
+toList (Maptrix m) = [ ((k1, k2), v)
+                     | (k1, vv) <- M.toList m
+                     , (k2, v) <- M.toList vv
+                     ]
+
+fromList :: (Ord k1, Ord k2, Eq v, Num v)
+         => [((k1, k2), v)]
+         -> Maptrix k1 k2 v
+fromList = foldl' go new
+  where go m ((k1, k2), v) = insert (S k1, S k2) v m
+
+rows :: (Ord k1, Ord k2, Eq v, Num v)
+     => Maptrix k1 k2 v
+     -> [k1]
+rows (Maptrix m) = M.keys m
 
 test =
   let m = (Maptrix M.empty) :: Maptrix String String Int
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings, TypeFamilies, FlexibleContexts #-}
 module Main where
 
 import Prelude hiding (readFile, log)
@@ -9,7 +9,7 @@ import Text.Printf
 import System.IO (withFile, IOMode(..), SeekMode(..), hSeek)
 
 import Data.Monoid
-import Data.SparseMaptrix
+import qualified Data.SparseMaptrix as Mx
 import Control.Applicative
 import Options.Applicative
 import Control.Monad.Reader
@@ -20,15 +20,18 @@ import qualified Crypto.Hash as CR
 import Data.String
 import qualified System.Directory as Dir
 import qualified System.Directory.Tree as Dir
+import System.FilePath (takeDirectory, takeBaseName, (</>))
 import qualified Data.Text.Encoding as Txt
 import qualified Data.Text as Txt
 import qualified Data.Map as M
 import Control.Monad.Logic
 import Control.Monad.Plus
-import Data.List (foldl1')
+import Data.List (foldl1', foldl', nub)
 
 import Data.Interval
 import qualified Data.IntervalMap.Lazy as IM
+
+import Debug.Trace
 
 intInt :: Integer -> Integer -> Interval Integer
 intInt a b = Finite a <=..< Finite b
@@ -42,6 +45,8 @@ data Config =
     { _copy           :: Bool
     , _verbosity      :: Int
     , _dryRun         :: Bool
+    , _confidenceThreshold :: Float
+    , _numSamples     :: Int
     , _torrentHook
     , _fileHook       :: Hook
     , _outDir
@@ -55,7 +60,8 @@ data FromFile =
     , _fromSize :: Integer
     } deriving (Show)
 
-type ToFiles = IM.IntervalMap Integer (Interval Integer, FilePath)
+type ToFile = (Interval Integer, FilePath)
+type ToFiles = IM.IntervalMap Integer ToFile
 
 data Match = FilePath :-> FilePath
              deriving (Show)
@@ -66,10 +72,17 @@ cliOptions = Config <$>
                     <> short 'C'
                     <> help "copy files instead of moving") <*>
                option auto (long "verbose"
-                         <> short 'v') <*>
+                         <> short 'v'
+                         <> value 1) <*>
                switch (long "pretend"
                     <> short 'p'
                     <> help "dry run: don't move or copy any files") <*>
+               option auto (long "confidence"
+                           <> short 'c'
+                           <> value 1) <*>
+               option auto (long "samples"
+                           <> short 'N'
+                           <> value 1) <*>
                strOption (long "torrent-hook"
                        <> value ""
                        <> help "Execute shell command for each processed torrent") <*>
@@ -92,7 +105,7 @@ loadTorrents =
     readTorrent' path = do
       bin <- lift $ B.readFile path
       case readTorrent bin of
-        Right b  -> return [(path, b)]
+        Right b  -> return [(takeBaseName path, b)]
         Left err -> log 0 (printf "Couldn't load %s: %s" path (show err)) >>
                     return []
   in do
@@ -110,13 +123,34 @@ torrentFiles torrent =
                                  , IM.insert int (int, path) acc
                                  )
         where path = bslToFilePath $ filePath tFile
-              len = fileLength tFile              
+              len = fileLength tFile
               int = offset `intInt` (offset + len)
   in
     snd $ foldl toFile (0, IM.empty) $ torrentFiles' torrent
 
-mkSizeIndex :: [FromFile] -> M.Map Integer [FromFile]
+type SzMatrix = Mx.Maptrix (FilePath, FilePath) FilePath Float
+
+type SzIndex = M.Map Integer [FromFile]
+
+mkSizeIndex :: [FromFile] -> SzIndex
 mkSizeIndex = foldl (\m v -> M.insertWith (++) (_fromSize v) [v] m) M.empty
+
+makeMatrix :: SzIndex
+           -> [(FilePath, Torrent)]
+           -> SzMatrix
+makeMatrix fromFiles torrents =
+  let
+    szLookup a = case a `M.lookup` fromFiles of
+                   Nothing -> []
+                   Just x -> map _fromPath x
+
+    sizeMatches = [ (((root, bslToFilePath toPath), fromPath), 1)
+                  | (root, torrent) <- torrents
+                  , TorrentFile sz toPath <- torrentFiles' $ tInfo torrent
+                  , fromPath <- szLookup sz
+                  ]
+  in
+    Mx.fromList sizeMatches
 
 bslToFilePath = Txt.unpack . Txt.decodeUtf8 . B.toStrict . B.intercalate "/"
 
@@ -158,66 +192,113 @@ zeropad n b =
   in if d <= 0
      then b'
      else B.append b' (B.replicate d 0)
-        
--- | Thorouthly check hashes of all pieces. The slowest
--- algorithm
-preciseMatch :: M.Map Integer [FromFile]
-             -> (FilePath, Torrent)
-             -> MyMonad [Match]
-preciseMatch fromFiles (torrentPath, Torrent{tInfo = torrent}) =
+
+type Atom = (Interval Integer, FilePath)
+
+filesInPiece :: ToFiles
+             -> Interval Integer
+             -> [Atom]
+filesInPiece toFiles piece = [ (interval, path)
+                             | (interval, (_, path)) <- IM.toList $ toFiles `IM.intersection` piece'
+                             ]
+  where piece' = IM.singleton piece (error "Should not happen (famous last words)")
+
+-- Create list of file pieces to check:
+pickBlocks :: Integer
+           -> Integer
+           -> ToFiles
+           -> [(Integer, [Atom])]
+pickBlocks numPieces pieceSize toFiles =
+  let
+    go i f | i == numPieces = []
+           | otherwise      = c ++ go (i+1) f''
+      where thisPiece = (offset `intInt` (offset + pieceSize))
+            offset = pieceSize * i
+            f' = filesInPiece toFiles thisPiece
+            f'' = [b | (_, b) <- f']
+            c = if f'' == f
+                then []
+                else [(i, f')]
+  in nub $ go 0 []
+
+blockMatchP :: B.ByteString
+            -> [Atom]
+            -> LogicT (ReaderT Config IO) ()
+blockMatchP hash atoms = do
+  piece <- lift $ B.concat <$> mapM (uncurry readInterval) atoms
+  assertEqual (show atoms) hash (sha1 piece)
+
+doBlock :: B.ByteString
+        -> FilePath
+        -> SzMatrix
+        -> (Integer, [Atom])
+        -> MyMonad SzMatrix
+doBlock hashes torrentPath mtx (i, toAtoms) =
+  let
+    combine (_, a) (_, b) = (((torrentPath, a), b), 1)
+
+    hash = B.take 20 $ B.drop (fromIntegral $ i*20) $ hashes
+
+    candidates to = (M.keys $ mtx Mx.! (Mx.S (torrentPath, to), Mx.H)) :: [FilePath]
+  in do
+    results <- observeAllT $ do
+      fromAtoms <- forM toAtoms $ \(interval, toPath) -> do
+                                    fromPath <- msum $ map pure $ candidates toPath
+                                    return (interval, fromPath)
+      blockMatchP hash fromAtoms
+      return $ zipWith combine toAtoms fromAtoms
+    return $ foldl (Mx.updateWith (+)) mtx results
+
+-- | Match real files with files in a torrent file
+doTorrent :: SzMatrix
+          -> (FilePath, Torrent)
+          -> MyMonad SzMatrix
+doTorrent mtx (torrentPath, Torrent{tInfo = torrent}) =
   let
     hashes    = tPieces torrent
     numPieces = (fromIntegral $ B.length hashes) `div` 20
     pieceSize = tPieceLength torrent
-    toFiles   = torrentFiles torrent
-
-    hash i = B.take 20 $ B.drop (fromIntegral $ i * 20) hashes
-
-    sameSize int = mfromList =<< mfromMaybe (M.lookup (width int) fromFiles)
-
-    tryFiles candidates hash = do
-      fromFiles <- mapM (liftM _fromPath . sameSize . fst . snd) candidates
-      let atoms' = zip (map fst candidates) fromFiles
-      piece <- B.concat <$> mapM (uncurry readInterval) atoms'
-      assertEqual (printf "files=%s" (show candidates)) hash (sha1 piece)
-      let candidates' = [path | (int, (trueInt, path)) <- candidates
-                              , upperBound trueInt == upperBound int
-                              ]
-      return $ zipWith (:->) fromFiles candidates'
-
-    go i acc
-        | i == numPieces = return acc
-        | otherwise      =
-            let
-              offset = i * pieceSize
-              pieceEnd = offset + pieceSize
-              piece = IM.singleton (offset `intInt` pieceEnd)
-                                   (error "Should not happen (famous last words)")
-              files = IM.toList $ toFiles `IM.intersection` piece
-            in ifte (tryFiles files $ hash i)
-                    (\match -> go (i+1) $ match ++ acc)
-                    (go (i+1) acc)
+    pieces    = pickBlocks numPieces pieceSize $ torrentFiles torrent
   in do
-    log 3 (printf "%s: pieceSize=%d numPieces=%d"
-                  torrentPath
-                  pieceSize
-                  numPieces)
-    log 8 (printf "%s files: %s"
-                  torrentPath
-                  (show toFiles))
-    observeT $ go 0 []
-                             
+    log 4 (printf "Checking %d/%d blocks in %s" (length pieces) numPieces torrentPath)
+    log 6 (printf "Pieces: %s" $ show pieces)
+    foldM (doBlock hashes torrentPath) mtx pieces
+
 main :: IO ()
 main = do
   config <- execParser $ info (helper <*> cliOptions) fullDesc
   (flip runReaderT) config $ do
-    fromFiles <- findFromFiles    
+    fromFiles <- findFromFiles
     log 7 (printf "fromFiles: %s" $ show fromFiles)
     torrents <- loadTorrents
     let sizeIndex  = mkSizeIndex fromFiles
-    matches <- concat <$> mapM (preciseMatch sizeIndex) torrents
-    log 1 (printf "Found %d matches" $ length matches)
-    forM_ matches $ \(a :-> b) -> log 2 (printf "%s -> %s" a b)
+        sizeMatrix = makeMatrix sizeIndex torrents
+        initialMatches = length $ Mx.toList sizeMatrix
+        ambiguous = [ r | r <- Mx.rows sizeMatrix
+                        , (>1) $ M.size $ sizeMatrix Mx.! (Mx.S r, Mx.H)]
+    log 3 (printf "Total number of files: %d" $ length fromFiles)
+    log 3 (printf "Matching sizes: %d" initialMatches)
+    log 3 (printf "Ambiguous cases: %d" $ length ambiguous)
+    log 4 (printf "Ambiguous: %s" $ show ambiguous)
+    log 7 (printf "preliminaryMatches: %s" $ show $ Mx.toList sizeMatrix)
+    sizeMatrix' <- foldM doTorrent sizeMatrix torrents
+    let matches = [(k2 :-> (root ++  '/':k1)) | (((root, k1), k2), v) <- Mx.toList sizeMatrix'
+                                              , v >= _confidenceThreshold config
+                                              ]
+    log 2 (printf "Triaged: %d" $ length matches)
+    lift $ completeWork config matches
+
+completeWork :: Config -> [Match] -> IO ()
+completeWork cfg matches
+  | _dryRun cfg = forM_ matches $ \(a :-> b) -> (printf "%s -> %s\n" a b)
+  | otherwise   =
+  let
+    action = Dir.copyFile
+  in forM_ matches $ \(from :-> to) -> do
+       let dest = _outDir cfg </> to
+       Dir.createDirectoryIfMissing True $ takeDirectory dest
+       printf "Copying %s to %s...\n" from dest
+       action from dest
 
 log :: Int -> String -> MyMonad ()
 log severity str = do
